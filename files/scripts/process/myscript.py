1593b6b30b30b3fdc676f1f7fcf37e11940051be
Graeme Grimes <11029039+ggrimes@users.noreply.github.com>
2022-03-11 15:24:16 +0000

    Create myscript.py

---
+++ files/scripts/process/myscript.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+    import gzip
+
+    reads = 0
+    bases = 0
+
+    with gzip.open('${launchDir}/data/yeast/reads/ref1_1.fq.gz', 'rb') as read:
+      for id in read:
+          seq = next(read)
+          reads += 1
+          bases += len(seq.strip())
+          next(read)
+          next(read)
+
+    print("reads", reads)
+    print("bases", bases)
