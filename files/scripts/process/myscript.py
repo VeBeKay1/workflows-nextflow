a5f1975b6828df7ef7e73a1d97e01cababc04cd8
Graeme Grimes <11029039+ggrimes@users.noreply.github.com>
2022-03-11 15:34:16 +0000

    Update myscript.py

--- files/scripts/process/myscript.py
+++ files/scripts/process/myscript.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
-    import gzip
+import gzip
+   reads = 0
+   bases = 0
 
-    reads = 0
-    bases = 0
-
-    with gzip.open('/home/training/data/yeast/reads/ref1_1.fq.gz', 'rb') as read:
-      for id in read:
-          seq = next(read)
-          reads += 1
-          bases += len(seq.strip())
-          next(read)
-          next(read)
-
-    print("reads", reads)
-    print("bases", bases)
+with gzip.open('/home/training/data/yeast/reads/ref1_1.fq.gz', 'rb') as read:
+  for id in read:
+      seq = next(read)
+      reads += 1
+      bases += len(seq.strip())
+      next(read)
+      next(read)
+print("reads", reads)
+print("bases", bases)
